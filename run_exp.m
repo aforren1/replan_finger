9e8697e0db04a7ed9dd8c3a778cb4377a08da6b6
Alex Forrence <aforren1@jhu.edu>
2017-02-08 13:43:24 -0500

    Outline of state machine

--- run_exp.m
+++ run_exp.m
@@ -2,7 +2,43 @@ function dat = run_exp(file_name, fullscreen)
 
 try
     setup_exp;
+    info_txt.Draw();
+    win.Flip();
+    WaitSecs(1);
 
+    for ii = fliplr(1:5)
+        helptext = ['Experiment starting in\n', ...
+                    num2str(ii), ' seconds'];
+        info_txt.Set('value', helptext);
+        info_txt.Draw();
+        win.Flip;
+        WaitSecs(1);
+    end
+
+    % set various flags
+    trial_count = 1;
+    state = 'pretrial';
+
+    % state machine
+    while true
+        if trial_count > length(tgt.trial)
+            % end of experiment
+            break;
+        end
+        % get the most recent presses/releases
+        [~, presses] = kbrd.Check;
+        
+        % state machine
+        switch state
+            case 'pretrial'
+            case 'intrial' 
+            case 'posttrial'
+            case 'feedback'
+        end
+        
+    end
+
+    % cleanup
 
 % bail out gracefully
 catch err 
