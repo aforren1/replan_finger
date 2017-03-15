1f1b1c7ce5cff3e60e9ed43187979bdb894fa4a7
Alex Forrence <aforren1@jhu.edu>
2017-03-15 15:13:09 -0400

    functionize mk_all_tfiles

--- tfiles/mk_all_tfiles.m
+++ tfiles/mk_all_tfiles.m
@@ -1,23 +1,29 @@
-% generate all finger pairs
-subject_id = '001';
-row = 4; % number from 1-6
-all_comb = [1 3 8 10; 1 4 7 10; 1 5 6 10; 2 4 7 9; 2 5 6 9; 3 5 6 8];
+function mk_all_tfiles(subject_id, row)
+% subject_id is a string
+% row is 1-6
+% pairs are:
+% [1 3 8 10; 1 4 7 10; 1 5 6 10; 2 4 7 9; 2 5 6 9; 3 5 6 8]
 
-%this_comb = all_comb(randperm(size(all_comb, 1), 1),:);
-this_comb = all_comb(row, :);
-dir_stub = ['tfiles/', subject_id, '/'];
-if ~exist(dir_stub, 'dir')
-    mkdir(dir_stub);
-end  
-% make a warmup block
-mk_tfiles(dir_stub, ['demo'], 'ind_finger', this_comb, ...
-          'transition_prob', 0, 'seed', 1, ...
-          'approx_trials', 50);
+    % generate all finger pairs
+    % number from 1-6
+    all_comb = [1 3 8 10; 1 4 7 10; 1 5 6 10; 2 4 7 9; 2 5 6 9; 3 5 6 8];
 
-for i = 1:15
-    mk_tfiles(dir_stub, ['block', num2str(i)], ...
-            'ind_finger', this_comb, ...
-            'approx_trials', 100, ...
-            'transition_prob', 0.3, ...
-            'seed', i);
+    %this_comb = all_comb(randperm(size(all_comb, 1), 1),:);
+    this_comb = all_comb(row, :);
+    dir_stub = ['tfiles/', subject_id, '/'];
+    if ~exist(dir_stub, 'dir')
+        mkdir(dir_stub);
+    end  
+    % make a warmup block
+    mk_tfiles(dir_stub, ['demo'], 'ind_finger', this_comb, ...
+            'transition_prob', 0, 'seed', 1, ...
+            'approx_trials', 50);
+
+    for i = 1:15
+        mk_tfiles(dir_stub, ['block', num2str(i)], ...
+                'ind_finger', this_comb, ...
+                'approx_trials', 100, ...
+                'transition_prob', 0.3, ...
+                'seed', i);
+    end
 end
